4c141ad2f596ea1b0e4a9fbae1822dbf406a885c
Leticia-maria <leticiapequeno30@gmail.com>
2024-05-12 20:18:38 -0400

    Add energies

--- src/rhf.jl
+++ src/rhf.jl
@@ -31,3 +31,28 @@ plot(sizes, times, label="Time", xaxis="Dimension", yaxis="Time (ns)", legend=:t
 savefig("indices_time.png")
 plot(sizes, memory, label="Memory", xaxis="Dimension", yaxis="Memory (bytes)", legend=:topleft)
 savefig("indices_memory.png")
+
+function scalingenergies(fun::Function, file)
+    totalenergies = []
+    repulsion = []
+
+    s = ["sto-2g", "sto-3g", "sto-6g", "def2-svp", "def2-tzvp"]
+
+    sizes = [2, 3, 4, 5, 6]
+
+    for dim in s
+        mol = molecule(file)
+        basis = parsebasis(mol, dim)
+        res = fun(basis, mol)
+        push!(totalenergies, res.total)
+        push!(repulsion, res.repulsion)
+        println("Energy: ", res.energy)
+        println("Total Energy: ", res.total)
+    end
+
+    return sizes, totalenergies, repulsion
+end
+
+sizes, totalenergies, repulsion = scalingenergies(rhf, "/Users/leticiamadureira/OohataHuzinaga.jl/test/data/hydrogen/h2.xyz")
+plot(totalenergies, sizes, label="Total Energies", xaxis="Dimension", yaxis="Total Energies", legend=:topleft)
+savefig("indices_energies.png")
